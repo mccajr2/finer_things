6c8aeb331deedb13843cd674eff0c42dd5ef6268
Jason McCarthy <mccarthy.jay@gmail.com>
2011-06-14 20:41:48 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FinerThings::Application.config.secret_token = '098ebe79ad82a87a1730c90f53a934047d3bbd3939050b636c9cfc27c74dd0f592dcb7619d2a7e9a2641de07bb3b227cb0020d87ffe89572b8db1e67119f89fe'
